842e11190e203bb6eeb56fc1bfdcc5af68353a26
laukik29 <laukiknakhwa@gmail.com>
2024-01-29 12:22:53 +0530

    Add

---
+++ CharEx2.java
@@ -0,0 +1,11 @@
+class CharEx2 {
+    public static void main(String args[]) {
+        char a ='A';
+        char b = 66;
+        a++;
+        b+=1;
+        System.out.println("a =" + a + ", b =" + b);
+        // output a =B, b =C
+        // if a=a+1 gives an error
+    }
+}
