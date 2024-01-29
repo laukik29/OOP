ef53ba9c396215193d363dc5759c65bc69713a66
laukik29 <laukiknakhwa@gmail.com>
2024-01-29 13:22:47 +0530

    classes and objects

---
+++ Arrays/Array.java
@@ -0,0 +1,9 @@
+// int[] age;
+// age = new int[5]
+class Array {
+    public static void main(String args[]) {
+        // int[] age = new int[5];
+        int[] age = {10,5,30,25,40};
+        System.out.println(age[0]); // Accessing the first element of the array
+    }
+}
---
+++ Arrays/example1.java
@@ -0,0 +1,12 @@
+class example1 {
+    public static void main(String args[]) {
+        int[] age = {1,5,2,7,8};
+        for(int i=0; i<5;i++)
+        // for(int i=0; i<age.length();i++)
+         {
+
+        
+        System.out.println("Element at index "+ i +" is " + age[i]);
+        }
+    }
+}
